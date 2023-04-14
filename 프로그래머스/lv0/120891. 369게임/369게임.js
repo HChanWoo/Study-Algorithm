0ed34a8ca525bb3312a28cdbe8f8a5c4ecd31d4a
황찬우 <49116370+HChanWoo@users.noreply.github.com>
2023-04-14 15:56:52 +0900

    [level 0] Title: 369게임, Time: 0.07 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,8 +1,3 @@
 function solution(order) {
-    
-    const sum_ = String(order).split('').reduce( (acc,cur)=>{
-        if(['3','6','9'].includes(cur)) return acc+1
-        else return acc
-    },0)
-    return sum_
+    return sum_ = String(order).split('').reduce( (acc,cur)=>{ if(['3','6','9'].includes(cur)) {return acc+1} else {return acc} },0)
 }
