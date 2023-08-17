224536fccf77688e07a3fc5211ef8baeeff9f67b
황찬우 <49116370+HChanWoo@users.noreply.github.com>
2023-08-17 20:57:39 +0900

    [level 2] Title: 피로도, Time: 0.13 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -1,17 +1,20 @@
 def solution(k, dungeons): 
     global answer
     answer = -1
-    visited = [0]*len(dungeons)
+    # 방문여부 리스트
+    visited = [0] * len(dungeons)
     dfs(k,dungeons,visited,0)
     return answer
 
-def dfs(k,dungeons,visited,cnt) :
+def dfs(k, dungeons,visited, cnt) :
     global answer
-    if answer < cnt :
-        answer = cnt
     
-    for idx in range(len(dungeons)) :
-        if k >= dungeons[idx][0] and not visited[idx] :
-            visited[idx]=1
-            dfs(k-dungeons[idx][1],dungeons,visited,cnt+1)
-            visited[idx]=0
+    # 방문한 던전의 수 업데이트
+    if answer < cnt : answer=cnt
+
+    for i in range(len(dungeons)) :
+        # 현재 필요도로 방문 가능하고, 방문하지 않은 경우 방문
+        if k >= dungeons[i][0] and not visited[i]:
+            visited[i]=1
+            dfs(k-dungeons[i][1],dungeons,visited,cnt+1)
+            visited[i]=0
