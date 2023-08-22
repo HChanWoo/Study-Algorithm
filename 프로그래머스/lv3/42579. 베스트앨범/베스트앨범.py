776321b7932562c86b7bd21efc8ea98240cfeeb7
황찬우 <49116370+HChanWoo@users.noreply.github.com>
2023-08-22 21:15:15 +0900

    Update 베스트앨범.py

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -1,8 +1,8 @@
 def solution(genres, plays):
     answer = []
 
-    dict_genre = {}
-    dict_plays = {}
+    dict_genre = {} # 장르별 총 재생수, {장르 : 총 재생 수}
+    dict_plays = {} # 장르별 고유번호별 재생수, {장르 : [[고유번호:재생 수]]}
 
     for i in range(len(genres)) :
         if genres[i] in dict_genre :
@@ -12,9 +12,10 @@ def solution(genres, plays):
             dict_genre[genres[i]] = plays[i]
             dict_plays[genres[i]] = [[i,plays[i]]]
 
-    print(dict_genre,dict_plays)
-    sorted_dict = dict(sorted(dict_genre.items(), key=lambda item: item[1], reverse=True))
+    # 총 재생수 내림차순 정령
+    sorted_dict = dict(sorted(dict_genre.items(), key=lambda item: -item[1]))
 
+    # 개별 재생 수 내림차순 정렬 후 최대 2개 answer에 추가
     for key in sorted_dict :
         tmp = sorted(dict_plays[key],key=lambda a:-a[1])
         answer+=tmp[:2]
