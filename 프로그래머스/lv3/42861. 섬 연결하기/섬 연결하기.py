c87c730c0de4d59108037d612c271e14471df1ef
황찬우 <49116370+HChanWoo@users.noreply.github.com>
2023-09-26 20:26:26 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,16 @@
+def solution(n, costs):
+    answer = 0
+    connected = set([costs[0][0]])
+    sorted_costs = sorted(costs, key = lambda x:x[2])
+    
+    while len(connected) != n :
+        for a1,b1,cost in sorted_costs :
+            if a1 in connected and b1 in connected :
+                continue
+            if a1 in connected or b1 in connected :
+                connected.add(a1)
+                connected.add(b1)
+                answer+=cost
+                break
+        
+    return answer
