9f5947fcd684b94447a87d77ea12e6f82d38e33f
황찬우 <49116370+HChanWoo@users.noreply.github.com>
2025-08-05 22:46:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 93.37 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,15 +1,26 @@
 function solution(participant, completion) {
     var answer = '';
-    const map = new Map();
-    participant.forEach((name) => {
-        map.set(name,(map.get(name) || 0) +1);
-    });
-    completion.forEach((name) => {
-        map.set(name,map.get(name)-1);
-    });
-    
-    for (const [key, value] of map) {
-      if(value > 0) return key;
+    participant.sort();
+    completion.sort();
+    for(var i = 0 ; i < participant.length; i++){
+        if(participant[i] !== completion[i]){
+            answer = participant[i];
+            break;
+        }
     }
     return answer;
+    
+//     var answer = '';
+//     const map = new Map();
+//     participant.forEach((name) => {
+//         map.set(name,(map.get(name) || 0) +1);
+//     });
+//     completion.forEach((name) => {
+//         map.set(name,map.get(name)-1);
+//     });
+    
+//     for (const [key, value] of map) {
+//       if(value > 0) return key;
+//     }
+//     return answer;
 }
