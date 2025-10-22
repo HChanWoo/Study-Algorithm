2ce81617d4ee72ca43a8913cb2c70acfb1be0507
황찬우 <49116370+HChanWoo@users.noreply.github.com>
2025-10-23 00:45:53 +0900

    [Gold V] Title: 짱해커 이동식, Time: 196 ms, Memory: 25812 KB -BaekjoonHub

--- 백준/Gold/25603. 짱해커 이동식/짱해커 이동식.js
+++ 백준/Gold/25603. 짱해커 이동식/짱해커 이동식.js
@@ -5,20 +5,16 @@ const input = fs.readFileSync(filePath).toString().trim().split("\n");
 const [N, K] = input[0].split(" ").map(Number);
 const costs = input[1].split(" ").map(Number);
 let left = 0, right = K-1;
-let answer = [];
+let max = -Infinity;
 
-if(N<=K) {
-    console.log(Math.min(...costs));
-} else {
-    while(left + K <= N) {
-        const window = costs.slice(left, right + 1);
-        const min = Math.min(...window);
-        const pos = left + window.lastIndexOf(min); // 현재 구간 내 위치
+while(left + K <= N) {
+    const window = costs.slice(left, right + 1);
+    const min = Math.min(...window);
+    const pos = left + window.lastIndexOf(min);
 
-        answer.push(min);
+    max = Math.max(max, min);
 
-        left = pos + 1;
-        right = Math.min(left + K - 1, N - 1);
-    }
-    console.log(Math.max(...answer));
+    left = pos + 1;
+    right = Math.min(left + K - 1, N - 1);
 }
+console.log(max);
